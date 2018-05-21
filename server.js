ddceba6b90bf0dc050c5ebf69887991a6b2bd600
Gyles <ghiles75@gmail.com>
2018-05-21 21:31:48 +0200

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express   = require('express');
 const path      = require('path');
 const app       = express();
-var httProxy    = require('http-proxy');
+var httpProxy   = require('http-proxy');
 var apiProxy    = httpProxy.createProxyServer();
 
 // Serve only the static files form the dist directory
