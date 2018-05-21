56bbc6b08d8f6a6a526f0fb98d732f233a2803d9
Gyles <ghiles75@gmail.com>
2018-05-21 22:54:25 +0200

    update server.js

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ const express   = require('express');
 const path      = require('path');
 const app       = express();
 var httpProxy   = require('http-proxy');
-var apiProxy    = httpProxy.createProxyServer();
+var apiProxy    = httpProxy.createProxyServer({ https: true, changeOrigin: true, secure: false, headers: { host: 'https://raphael.cfapps.io'} });
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Raphael'));
 
+// Use proxy
+app.all('/oauth/**', function(req, res) {
+    console.log('redirecting proxy to server');
+    apiProxy.web(req, res, { target: 'https://raphael.cfapps.io' });
+});
+
+app.all('/api/v1/**', function(req, res) {
+    console.log('redirecting proxy to server');
+    apiProxy.web(req, res, { target: 'https://raphael.cfapps.io' });
+});
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname+'/dist/Raphael/index.html'));
-});
-
-// Use proxy
-app.all("/api/v1/**", function(req, res) {
-    console.log('redirecting proxy to server');
-    apiProxy.web(req, res, { target: 'https://raphael.cfapps.io' });
-});
-
-app.all("/oauth/*", function(req, res) {
-    console.log('redirecting proxy to server');
-    apiProxy.web(req, res, { target: 'https://raphael.cfapps.io' });
 });
 
 // Start the app by listening on the default Heroku port
